593dfe96f240514abe9b75094eb6e2242c262db4
TarekZSh <tarekshawahni.25@gmail.com>
2023-10-03 00:43:06 +0300

    fans background page

--- BneiSakhninApp/BneiSakhninApp/MainView.swift
+++ BneiSakhninApp/BneiSakhninApp/MainView.swift
@@ -11,51 +11,23 @@ struct MainView: View {
                     .scaledToFill()
                     .ignoresSafeArea(.all)
                     .offset(CGSize(width: -55, height: 0))
+                
                 VStack{
                     Spacer()
                     HStack{
-                        Button{
-                            isClicked.toggle()
-                        }label: {
+                        NavigationLink(destination:
+                                        {
+                            MainView1()
+                        },label: {
                             Text("Next")
                                 .foregroundColor(.white)
                                 .font(.system(size: 50))
                                 .padding(.horizontal,160)
                                 .background(Color("RedSakh"))
-                        }
-                    }
-                    
-                }
-                
-                 /*
-                VStack {
-                    Button(action:
-                        {
-                        isClicked.toggle()
                         })
-                        {
-                        HStack(spacing: 24)
-                            {
-                            Text("Next")
-                            }
-                            .frame(width: UIScreen.main.bounds.width,
-                        height:55)
-                        .foregroundColor(.white)
-                        .background(Color("RedSakh"))
-                        .padding(.top, 790)
-                        .fontWeight(.black)
-                        .font(.system(size: 26))
-                        }
-                    
-                    NavigationLink(
-                        destination: MainView1().navigationBarBackButtonHidden(true),
-                        isActive: $isClicked
-                    )
-                    {
-                        EmptyView()
                     }
+                    
                 }
-                  */
             }
         }
     }
